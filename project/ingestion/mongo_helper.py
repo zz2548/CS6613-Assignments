133e82460f8d7c63d88ead28638bf8b7e34f1ef8
Jerry Zou <zz2548@nyu.edu>
2025-12-08 02:08:47 +0000

    M4 Output complete

--- project/ingestion/mongo_helper.py
+++ project/ingestion/mongo_helper.py
@@ -19,21 +19,10 @@ class MongoHelper:
         }
         return self.db.webpages.insert_one(doc)
     
-    def insert_video(self, url: str, title: str, transcript: str, metadata: Dict = None):
-        """Insert video data"""
-        doc = {
-            'url': url,
-            'title': title,
-            'transcript': transcript,
-            'type': 'video',
-            'metadata': metadata or {},
-            'ingested_at': datetime.now()
-        }
-        return self.db.videos.insert_one(doc)
-    
-    def insert_slide(self, filename: str, content: str, metadata: Dict = None):
+    def insert_slide(self, url: str, filename: str, content: str, metadata: Dict = None):
         """Insert slide/PDF data"""
         doc = {
+            'url': url,
             'filename': filename,
             'content': content,
             'type': 'slide',
@@ -47,23 +36,19 @@ class MongoHelper:
         urls = []
         for doc in self.db.webpages.find({}, {'url': 1}):
             urls.append(('webpage', doc['url']))
-        for doc in self.db.videos.find({}, {'url': 1}):
-            urls.append(('video', doc['url']))
-        for doc in self.db.slides.find({}, {'filename': 1}):
-            urls.append(('slide', doc['filename']))
+        for doc in self.db.slides.find({}, {'url': 1, 'filename': 1}):
+            urls.append(('slide', doc.get('url', doc['filename'])))
         return urls
     
     def count_documents(self):
         """Get count of documents in each collection"""
         return {
             'webpages': self.db.webpages.count_documents({}),
-            'videos': self.db.videos.count_documents({}),
             'slides': self.db.slides.count_documents({})
         }
     
     def clear_all(self):
         """Clear all collections"""
         self.db.webpages.delete_many({})
-        self.db.videos.delete_many({})
         self.db.slides.delete_many({})
         print("All collections cleared!")
